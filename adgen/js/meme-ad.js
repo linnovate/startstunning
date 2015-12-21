ad7c8da4a61e1c2637f0f7e93755241095946658
Alexander Borisenko <wealthybora@gmail.com>
2015-12-21 18:47:43 +0300

    extra check

--- adgen/js/meme-ad.js
+++ adgen/js/meme-ad.js
@@ -95,7 +95,7 @@ String.prototype.capitalize = function() {
         };
 
         component.init = function (success) {
-            $.getJSON(baseURL+'/data.json').done(function (data) {
+            $.getJSON(baseURL+'/data.json?').done(function (data) {
                 component.itemsData = data;
                 success.call(component);
             });
