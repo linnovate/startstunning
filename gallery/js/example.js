1dbbebb09b9cf28318a200e56b3ff395619ea10f
Alexander Borisenko <wealthybora@gmail.com>
2016-01-07 16:43:58 +0300

    simple colletion test

---
+++ gallery/js/example.js
@@ -0,0 +1,236 @@
+(function() {
+  window.Example = angular.module('Example', []);
+
+  window.Example.controller('example', function($scope) {
+    var addLoader, loadCollections, showError, updateModel;
+    $scope.collections = $cll;
+    $scope.items = [];
+    $scope.isLoading = true;
+    $cll.initializeAngular($scope);
+    addLoader = function(promise) {
+      $scope.isLoading = true;
+      return promise.always(function() {
+        return $scope.isLoading = false;
+      });
+    };
+    $cll.on(['any'], addLoader);
+    $cll.Collection.on(['any'], addLoader);
+    $cll.ItemsList.on(['any'], addLoader);
+    $cll.Item.on(['any'], addLoader);
+    showError = function(text) {
+      $scope.error = text;
+      return setTimeout(function() {
+        if ($scope.error === text) {
+          $scope.error = null;
+        }
+        return $scope.$apply();
+      }, 2000);
+    };
+    $cll.Collection.on(['any'], function(promise, event) {
+      return promise.fail(function() {
+        return showError("Collection " + event.name + " failed");
+      });
+    });
+    $cll.Item.on(['any'], function(promise, event) {
+      return promise.fail(function() {
+        return showError("Item " + event.name + " failed");
+      });
+    });
+    $cll.ItemsList.on(['any'], function(promise, event) {
+      return promise.fail(function() {
+        return showError("Items " + event.name + " failed");
+      });
+    });
+    loadCollections = function() {
+      return $cll.getAll().then(function(collections) {
+        if (collections.length) {
+          if (!$scope.selectedCollection || collections.length === 1) {
+            return $scope.selectCollection(collections[0]);
+          } else {
+            return _.find(collections, function(cll) {
+              return cll.id === $scope.selectCollection.id;
+            });
+          }
+        }
+      });
+    };
+    $scope.selectCollection = function(collection) {
+      $scope.selectedCollection = collection;
+      return $scope.selectedItems = [];
+    };
+    $scope.selectedItems = [];
+    $scope.selectItem = function(item) {
+      var removedItems;
+      removedItems = _.remove($scope.selectedItems, function(i) {
+        return i === item;
+      });
+      if (!removedItems.length) {
+        return $scope.selectedItems.push(item);
+      }
+    };
+    $scope.isSelectedItem = function(item) {
+      return !!_.find($scope.selectedItems, function(i) {
+        return i === item;
+      });
+    };
+    $scope.createCollection = function() {
+      var collection;
+      return collection = new $cll.Collection({
+        title: 'new collection',
+        items: [
+          {
+            title: '111'
+          }, {
+            title: '222'
+          }
+        ]
+      });
+    };
+    $scope.createItem = function() {
+      var item;
+      return item = new $cll.Item($scope.selectedCollection, {
+        title: 'new item'
+      });
+    };
+    $scope.addItemToStart = function() {
+      var item;
+      item = new $cll.Item($scope.selectedCollection, {
+        title: 'new item'
+      });
+      return item.moveToStart();
+    };
+    $scope.addItemToEnd = function() {
+      var item;
+      item = new $cll.Item($scope.selectedCollection, {
+        title: 'new item'
+      });
+      return item.moveToEnd();
+    };
+    $scope.addItemBeforeSelected = function() {
+      var item;
+      item = new $cll.Item($scope.selectedCollection, {
+        title: 'new item'
+      });
+      return item.moveBefore(_.last($scope.selectedItems));
+    };
+    $scope.addItemAfterSelected = function() {
+      var item;
+      item = new $cll.Item($scope.selectedCollection, {
+        title: 'new item'
+      });
+      return item.moveAfter(_.last($scope.selectedItems));
+    };
+    $scope.deleteCollection = function(collection) {
+      return collection["delete"]().then(function() {
+        var _ref;
+        if (((_ref = $scope.selectedCollection) != null ? _ref.id : void 0) === collection.id) {
+          $scope.selectCollection(null);
+          return $scope.selectedItems = [];
+        }
+      });
+    };
+    $scope.deleteItem = function(item) {
+      return item["delete"]().then(function() {
+        return $scope.selectedItems = [];
+      });
+    };
+    updateModel = function(model, textData) {
+      var data;
+      try {
+        data = JSON.parse(textData);
+        _.each(data, function(value, key) {
+          if (model[key] != null) {
+            model[key] = value;
+          }
+        });
+        return true;
+      } catch (_error) {
+        showError('JSON is invalid');
+        return false;
+      }
+    };
+    $scope.toEditableModelData = function(model) {
+      return JSON.stringify({
+        title: model.title,
+        tags: model.tags,
+        type: model.type,
+        thumbnailUrl: model.thumbnailUrl,
+        uniquePerUser: model.uniquePerUser,
+        privateProperties: model.privateProperties,
+        publicProperties: model.publicProperties
+      });
+    };
+    $scope.saveCollectionData = function(collection, newData) {
+      if (!updateModel(collection, newData)) {
+        return false;
+      }
+      return collection.save();
+    };
+    $scope.updateCollection = function(collection) {
+      if (!updateModel(collection, '{"title": "updatedCollection", "tags": ["test", "js"], "public_properties": {"author": "wix"}}')) {
+        return false;
+      }
+      return collection.update({
+        title: 'updatedCollection',
+        tags: ['test', 'js'],
+        public_properties: {
+          author: 'wix'
+        }
+      });
+    };
+    $scope.saveItemData = function(item, newData) {
+      if (!updateModel(item, newData)) {
+        return false;
+      }
+      return item.save();
+    };
+    $scope.moveItemToStart = function(item) {
+      return item.moveToStart();
+    };
+    $scope.moveItemToEnd = function(item) {
+      return item.moveToEnd();
+    };
+    $scope.moveItemBeforeSelected = function(item) {
+      return item.moveBefore(_.last($scope.selectedItems));
+    };
+    $scope.moveItemAfterSelected = function(item) {
+      return item.moveAfter(_.last($scope.selectedItems));
+    };
+    $scope.saveSelectedItems = function() {
+      return $scope.selectedCollection.items.save($scope.selectedItems);
+    };
+    $scope.moveToStartSelectedItems = function() {
+      return $scope.selectedCollection.items.moveToStart($scope.selectedItems);
+    };
+    $scope.moveToEndSelectedItems = function() {
+      return $scope.selectedCollection.items.moveToEnd($scope.selectedItems);
+    };
+    $scope.moveBeforeSelectedItems = function() {
+      return $scope.selectedCollection.items.moveBefore($scope.selectedItems, _.last($scope.selectedItems));
+    };
+    $scope.moveAfterSelectedItems = function() {
+      return $scope.selectedCollection.items.moveAfter($scope.selectedItems, _.last($scope.selectedItems));
+    };
+    $scope.deleteSelectedItems = function() {
+      return $scope.selectedCollection.items["delete"]($scope.selectedItems).then(function() {
+        return $scope.selectedItems = [];
+      });
+    };
+    return loadCollections();
+  });
+
+  window.Example.filter('json', function() {
+    return function(val) {
+      if (typeof val === "function") {
+        return 'function()';
+      } else if (val instanceof $cll.Collection) {
+        return "Collection#" + val.id;
+      } else if (val instanceof $cll.Item) {
+        return "Item#" + val.id;
+      } else {
+        return JSON.stringify(val);
+      }
+    };
+  });
+
+}).call(this);
