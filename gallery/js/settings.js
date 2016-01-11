6d57ee974cc38fdb1b17e574924cf517b5fc8999
Alexander Borisenko <wealthybora@gmail.com>
2016-01-11 14:29:37 +0300

    making adminManager

--- gallery/js/settings.js
+++ gallery/js/settings.js
@@ -1,37 +1,63 @@
 (function ($) {
 
-    var galSettings = function () {
+    var galManager = function () {
+        var that = this,
+            collection;
 
-        this.setVal = function (key, value) {
-            Wix.Styles.setFontParam(key, {
-                    value: value
-                }
-            );
+        that.setCollectionId = function (guid) {
+            Wix.Settings.setExternalId(guid, function () {
+            }, function() {
+                console.error('Can not save guid with setExternalId');
+            });
         };
 
-        this.getStyleParams = function (key) {
-            Wix.Styles.getStyleParams(function (styleParams) {
-                console.log('getStyleParams: ', styleParams);
+        that.getCollectionId = function (callback) {
+            Wix.getExternalId(callback);
+        };
+
+        /**
+         * Loads saved collection or creates an empty and saves
+         */
+        that.init = function () {
+            that.getCollectionId(function (id) {
+                if (_.isEmpty(id)) {
+                    // create an empty collection
+                    collection = new $cll.Collection({
+                        type: 'videoGallery',
+                        items: [],
+                        publicProperties: {
+
+                        }
+                    });
+                } else {
+                    $cll.get(id).then(function (loadedCollection) {
+                        console.log('just loaded collection', loadedCollection);
+
+                        collection = loadedCollection;
+                    });
+                }
             });
         };
     };
 
     $(document).ready(function () {
-        var adminTpl, generagedHTML;
+        var adminTpl,
+            generagedHTML,
+            gMan = new galManager;
 
-        //adminTpl = _.template($('.tplAdminForm').html());
-        //generagedHTML = adminTpl({items: slides});
-        //$('.wrapAdmin').html(adminTemplate);
-
-        Wix.UI.initialize({});
+        adminTpl = _.template($('.tplAdminForm').html());
+        generagedHTML = adminTpl({items: []});
+        $('.wrapAdmin').html(generagedHTML);
 
 //        Wix.UI.get('tiTle');
 //        Wix.UI.onChange('tiTle', function(value, key){
 //            Wix.UI.toJSON();
 //        });
 
+        Wix.UI.initialize({});
+
         $('.left .imgs .item').click(function () {
-            var ind = $(this).data('ind');
+            var ind = $(this).data('wix-item-id');
             var item = slides[ind];
             $('.inputWrap.description input').val(item.data_filmn);
             $('.inputWrap.title input').val(item.title);
@@ -47,14 +73,16 @@
         $('.newImage').click(function () {
             Wix.Settings.openMediaDialog(Wix.Settings.MediaType.IMAGE, false, function (data) {
                 console.log('image data: ', data);
-                // save image data
-            })
+
+                console.log('going to getCollectionId: ');
+                gMan.getCollectionId(function (id) {
+                    console.log('we got value: ', id);
+                });
+            });
+            /*console.log('going to setCollectionId');
+            gMan.setCollectionId('bb133804-47a9-40e8-955f-6c50149467ee');*/
         });
 
-
-        var insSet = new galSettings;
-        insSet.setVal('bora', 'wow!');
-        insSet.getStyleParams();
 
     });
 
